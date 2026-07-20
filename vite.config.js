30751fc15044f90c9c57855ea1b22f0ef361204a
minbanyartalahtaw <minbanyartalahtaw@proton.me>
2026-07-20 11:06:45 +0700

    url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/first_react_app/",
+  base: "/DELETE_WD_007_First_React_Project/",
   plugins: [react()],
   build: { outDir: "docs" },
 });
