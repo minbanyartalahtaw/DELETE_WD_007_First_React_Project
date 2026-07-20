86da4aa668d1d33b3f72445ae4d6b59c1da40f12
minbanyartalahtaw <minbanyartalahtaw@proton.me>
2026-07-20 11:51:34 +0700

    asdf

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -3,11 +3,8 @@ import "../App.css";
 function Greeting({ message }) {
   return (
     <>
-      <div className="card">
-        <h1>Hello React !</h1>
-        {""}
-        <p>{message}</p>
-      </div>
+      <h1>Hello React !</h1>
+      <p>{message}</p>
     </>
   );
 }
